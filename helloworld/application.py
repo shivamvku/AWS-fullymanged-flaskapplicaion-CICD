beac56c821859ad1196158eaa8432f73b2a41e6e
vineet kumar upadhyay <shivam.vku@gmail.com>
2021-06-10 17:14:24 +0530

    Update application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -9,9 +9,10 @@ application = Flask(__name__)
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
-@application.route('/', methods=['POST'])
+@application.route('/index', methods=['POST'])
 def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World from vineet'}), mimetype='application/json', status=200)
+
 
 if __name__ == '__main__':
     flaskrun(application)
